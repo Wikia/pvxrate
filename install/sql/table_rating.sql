ceb9737a8ae0ec8cf691a907bdc8347a9b2a7737
Alexia E. Smith <washuu@gmail.com>
2020-10-26 08:26:17 -0500

    Add some defaults to this table.  Having both NOT NULL and no default is weird.

--- install/sql/table_rating.sql
+++ install/sql/table_rating.sql
@@ -1,15 +1,20 @@
 CREATE TABLE /*_*/rating (
-    `rate_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
-    `page_id` int(8) NOT NULL,
-    `user_id` int(5) NOT NULL,
-    `comment` text NOT NULL,
-    `rollback` int(1) NOT NULL,
-    `admin_id` int(5) NOT NULL,
-    `reason` text NOT NULL,
-    `rating1` int(3) NOT NULL,
-    `rating2` int(3) NOT NULL,
-    `rating3` int(3) NOT NULL,
-    `ip_address` varchar(50) NOT NULL,
-    `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    UNIQUE KEY `rate_id` (`rate_id`)
+  `rate_id` int(5) UNSIGNED NOT NULL,
+  `page_id` int(8) NOT NULL DEFAULT '0',
+  `user_id` int(5) NOT NULL DEFAULT '0',
+  `comment` text NOT NULL,
+  `rollback` int(1) NOT NULL DEFAULT '0',
+  `admin_id` int(5) NOT NULL DEFAULT '0',
+  `reason` text NOT NULL,
+  `rating1` int(3) NOT NULL DEFAULT '0',
+  `rating2` int(3) NOT NULL DEFAULT '0',
+  `rating3` int(3) NOT NULL DEFAULT '0',
+  `ip_address` varchar(50) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) /*$wgDBTableOptions*/;
+
+ALTER TABLE /*_*/rating
+  ADD UNIQUE KEY `rate_id` (`rate_id`);
+
+ALTER TABLE /*_*/rating
+  MODIFY `rate_id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
